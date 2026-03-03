bf5b8a5dd8b6a2092671a2b55d1a52e73c9b1708
Nilzao <288571+nilzao@users.noreply.github.com>
2026-03-03 16:30:11 -0400

    automação de issues para template markdown

--- build-contrib.sh
+++ build-contrib.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 source formataNome.sh
 
-ANO="6o ano"
-DISCIPLINA="Ciências"
-TITULO_RESUMIDO="Ciclo das rochas e Eras Geológicas"
-TITULO_NBLM="Ciclo das Rochas e Eras Geológicas: transformações da Litosfera"
-LINK_NBLM="https://notebooklm.google.com/notebook/5c0f324a-6311-458e-ac76-adc95e6b750a"
-UNIDADE_TEMATICA_BNCC="Terra e Universo"
-OBJ_CONHECIMENTO_BNCC="Forma, estrutura e movimentos da Terra"
-COD_BNCC="EF06CI12"
-HABILIDADE_BNCC="Identificar diferentes tipos de rocha, relacionando a formação de fósseis a rochas sedimentares em diferentes períodos geológicos."
-
 aplicaTemplate() {
+  local ANO=$1
+  local DISCIPLINA=$2
+  local TITULO_RESUMIDO=$3
+  local TITULO_NBLM=$4
+  local LINK_NBLM=$5
+  local UNIDADE_TEMATICA_BNCC=$6
+  local OBJ_CONHECIMENTO_BNCC=$7
+  local COD_BNCC=$8
+  local HABILIDADE_BNCC=$9
 
-  local TITULO_RESUMIDO=$1
-  local  TITULO_NBLM=$2
-  local LINK_NBLM=$3
-  local  UNIDADE_TEMATICA_BNCC=$4
-  local OBJ_CONHECIMENTO_BNCC=$5
-  local   COD_BNCC=$6
-  local  HABILIDADE_BNCC=$7
-
-  local  TEMPLATE="template.md"
+  local DIR_ANO="${ANO//[^0-9]/}"
+  local DIR_DISCIPLINA=$(formataNome "${DISCIPLINA}")
+  local MD_TITULO_RESUMIDO=$(formataNome "${TITULO_RESUMIDO}")
+  local TEMPLATE="template.md"
 
   sed \
     -e "s|%TITULO_RESUMIDO%|${TITULO_RESUMIDO}|g" \
@@ -31,18 +25,5 @@ aplicaTemplate() {
     -e "s|%OBJ_CONHECIMENTO_BNCC%|${OBJ_CONHECIMENTO_BNCC}|g" \
     -e "s|%COD_BNCC%|${COD_BNCC}|g" \
     -e "s|%HABILIDADE_BNCC%|${HABILIDADE_BNCC}|g" \
-    "${TEMPLATE}"
+    "${TEMPLATE}" > "docs/${DIR_ANO}/${DIR_DISCIPLINA}/${MD_TITULO_RESUMIDO}.md"
 }
-
-DIR_ANO="${ANO//[^0-9]/}"
-DIR_DISCIPLINA=$(formataNome "${DISCIPLINA}")
-MD_TITULO_RESUMIDO=$(formataNome "${TITULO_RESUMIDO}")
-
-aplicaTemplate \
-  "${TITULO_RESUMIDO}" \
-  "${TITULO_NBLM}" \
-  "${LINK_NBLM}" \
-  "${UNIDADE_TEMATICA_BNCC}" \
-  "${OBJ_CONHECIMENTO_BNCC}" \
-  "${COD_BNCC}" \
-  "${HABILIDADE_BNCC}" > "docs/${DIR_ANO}/${DIR_DISCIPLINA}/${MD_TITULO_RESUMIDO}.md"
---
+++ issues.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+source build-contrib.sh
+
+source .issues-token.sh
+
+curl -s -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/ciencialine/caderno-aberto/issues?state=open&assignee=ciencialine" | \
+jq -c '.[] | {number, body}'  | while read -r item; do
+  number=$(echo "$item" | jq -r '.number')
+  body=$(echo "$item" | jq -r '.body')
+
+  readarray -d '' valores < <(echo "$body" | awk '
+  /^### / {
+      if (val != "") {
+          gsub(/^[ \t\n]+|[ \t\n]+$/, "", val)
+          printf "%s\0", val
+      }
+      val = ""
+      next
+  }
+  {
+      val = val (val=="" ? "" : "\n") $0
+  }
+  END {
+      if (val != "") {
+          gsub(/^[ \t\n]+|[ \t\n]+$/, "", val)
+          printf "%s\0", val
+      }
+  }')
+
+  echo "Processando issue #$number"
+  aplicaTemplate \
+    "${valores[0]//$'\n'/ }" \
+    "${valores[1]//$'\n'/ }" \
+    "${valores[2]//$'\n'/ }" \
+    "${valores[3]//$'\n'/ }" \
+    "${valores[4]//$'\n'/ }" \
+    "${valores[5]//$'\n'/ }" \
+    "${valores[6]//$'\n'/ }" \
+    "${valores[7]//$'\n'/ }" \
+    "${valores[8]//$'\n'/ }"
+
+done
